de7c77f5a8ad164e5fda420d5a4434d4969406ad
emilio-robres <emiliorobres@gmail.com>
2021-10-13 17:03:19 +0200

    Logro: Funciona el scrolling infinito, el remove foto y el responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+  const [albumId, setAlbumId] = useState(1);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      try {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=1`);
+        console.log(res.data);
+        setPhotos(res.data)
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getPhotos();
+  }, []);
+
+  const getMorePhotos = async () => {
+    try {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`);
+      console.log(res.data);
+      return (res.data)
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const fetchData = async () => {
+    const morePhotosFromServer = await getMorePhotos();
+
+    setPhotos([...photos, ...morePhotosFromServer]);
+
+    if (morePhotosFromServer.length === 0 || morePhotosFromServer.length < 50) {
+      sethasMore(false)
+    }
+    setAlbumId(albumId + 1)
+  };
+
+  const handleRemove = (id) => {
+    const filterPhotos = photos.filter(photo => photo.id !== id)
+    setPhotos(filterPhotos)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InfiniteScroll
+      dataLength={photos.length}
+      next={fetchData}
+      hasMore={hasMore}
+      loader={<h1 style={{ textAlign: 'center' }}>Loading...</h1>}
+      endMessage={<h1 style={{ textAlign: 'center' }}>No hay más fotos</h1>}
+    >
+      <div className="container">
+
+        {photos.map((item) =>
+          <div key={item.id} id={item.id} onClick={() => handleRemove(item.id)}>
+            <img
+              src={item.thumbnailUrl}
+              alt={"placerholder"}
+              loading="lazy"
+            />
+          </div>
+        )}
+      </div>
+    </InfiniteScroll>
   );
 }
+
+
 
 export default App;
